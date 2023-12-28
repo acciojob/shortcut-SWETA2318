e0b376d60bbde628ee80fb6d86292e9cefc5a072
SWETA2318 <150820032+SWETA2318@users.noreply.github.com>
2023-12-28 12:02:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 function shortcut(s1, s2) {
-  // your code here
+  // Check if either of the input strings is empty
+  if (s1 === "" || s2 === "") {
+    return "";
+  }
+
+  // Extract the initial letters and convert them to uppercase
+  const initialLetters = s1.charAt(0).toUpperCase() + s2.charAt(0).toUpperCase();
+
+  return initialLetters;
 }
 
-// Do not change the code below.
+// Prompt the user for input
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
+
+// Call the function and display the result
 alert(shortcut(s1, s2));
